e642c4253b017543ef2d34ce9069449a5cb76ba5
Alaina Rasheed <alrashee@ucsd.edu>
2017-02-22 19:14:17 -0800

    slide 40 deployment

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,17 @@ $(document).ready(function() {
 	initializePage();
 });
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".project").click(projectClick);
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    ga("send", "event", 'like', 'click');
 }
